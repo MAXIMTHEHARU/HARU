cd66d7c15f6edc1d48a04d3693792116a1778142
MAXIMTHEHARU <150433722+MAXIMTHEHARU@users.noreply.github.com>
2023-11-30 17:22:05 +0900

    계산기 만들기 Level 1

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,20 +1,23 @@
-package com.android.myapplicationdd
+package com.android.mycalculator
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.Button
+fun main() {
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    var number1 = readln().toDouble()
+    var operate = '+'
+    var number2 = readln().toDouble()
 
-        val nextButton = findViewById<Button>(R.id.btn_next)
+    operate = '-'
 
-        nextButton.setOnClickListener {
-            val intent = Intent(this, Intent_Activity::class.java)
-            startActivity(intent)
-        }
+
+    if (operate == '+') {
+        println("${number1 + number2}")
+    } else if (operate == '-') {
+        println("${number1 - number2}")
+    } else if (operate == '*') {
+        println("${number1 * number2}")
+    } else if (operate == '/') {
+        println("${number1 / number2}")
+    } else {
+        println("잘못된 연산자 입니다.")
     }
 }
