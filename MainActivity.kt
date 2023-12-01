2eee17a7cbe488dc4719f6b01ccd120352423ebf
MAXIMTHEHARU <150433722+MAXIMTHEHARU@users.noreply.github.com>
2023-12-01 10:37:37 +0900

    계산기 만들기 Level3333

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,23 +1,65 @@
 package com.android.mycalculator
 
 fun main() {
+    var num1 = readLine()!!.toDouble()
+    var operation = readLine()!!.toString()
+    var num2 = readLine()!!.toDouble()
 
-    var number1 = readln().toDouble()
-    var operate = '+'
-    var number2 = readln().toDouble()
+    var cal = Calculator()
 
-    operate = '-'
+    var result = {num1().operation().num2()}
+
+    println("${num1}${operation}${num2}의 값은 ${result}")
 
 
-    if (operate == '+') {
-        println("${number1 + number2}")
-    } else if (operate == '-') {
-        println("${number1 - number2}")
-    } else if (operate == '*') {
-        println("${number1 * number2}")
-    } else if (operate == '/') {
-        println("${number1 / number2}")
-    } else {
-        println("잘못된 연산자 입니다.")
+
+
+}
+
+class Calculator {
+    fun Operate(num1: Double, oper: String, num2: Double): String {
+
+        var sub = SubstractOperation()
+        var add = AddOperation()
+        var mul = MultiplyOperation()
+        var div = DivideOperation()
+        var result: String
+
+    }
+}
+
+class AddOperation {
+    fun operate(num1:Double, num2:Double): String {
+
+        var result: String
+        result = (num1 + num2).toString()
+        return result
+    }
+}
+
+class SubstractOperation {
+    fun operate(num1:Double, num2:Double): String {
+
+        var result: String
+        result = (num1-num2).toString()
+        return result
+    }
+}
+
+class MultiplyOperation {
+    fun operate(num1: Double, num2: Double): String {
+
+        var result: String
+        result = (num1 * num2).toString()
+        return result
+    }
+}
+
+class DivideOperation {
+    fun operate(num1: Double, num2: Double): String {
+
+        var result: String
+        result = (num1 / num2).toString()
+        return result
     }
 }
